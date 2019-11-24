3b2b3ade0952dde2017a76dd20e4fb069d1e266b
Seanicus1000 <965939@swansea.ac.uk>
2019-11-24 22:30:37 +0000

    <some kind of message>

---
+++ UserProfile.java
@@ -0,0 +1,71 @@
+/**
+ *
+ */
+public class UserProfile {
+
+    private String name;
+    private int highestLevel;
+
+    /**
+     * Create a user profile with a given name and their highest level.
+     * @param name The name of the user.
+     * @param highestLevel The highest level achieved by the user.
+     */
+    public UserProfile(String name, int highestLevel) {
+        this.name = name;
+        this.highestLevel = highestLevel;
+    }
+
+    /**
+     * Sets the name of the user.
+     * @param name The name of the user.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the name of the user.
+     * @return The name of the user.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the highest level achieved by the user.
+     * @param highestLevel The highest level achieved by the user.
+     */
+    public void setHighestLevel(int highestLevel) {
+        this.highestLevel = highestLevel;
+    }
+
+    /**
+     * Get the highest level achieved by the user.
+     * @return highestLevel The highest level achieved by the user.
+     */
+    public int getHighestLevel() {
+        return highestLevel;
+    }
+
+    public String toString() {
+        return name + ":" + highestLevel;
+    }
+
+    /**
+     * Outputs the result of to string
+     * @return //TODO: figure our what to put here lol
+     */
+    public String fromString() {
+        String[] output = toString().split(":");
+        for (String word : output) {
+            System.out.println(word);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        UserProfile t = new UserProfile("Jimmy", 5);
+        System.out.println(t.fromString());
+    }
+}
