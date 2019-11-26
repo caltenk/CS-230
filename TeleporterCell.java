5b97d19531474d618dc64e375b128778ebd98f18
GeorgeManning2000 <58079989+GeorgeManning2000@users.noreply.github.com>
2019-11-26 22:06:34 +0000

    Added JavaDoc

--- TeleporterCell.java
+++ TeleporterCell.java
@@ -1,17 +1,37 @@
-
+/**
+ * A teleporter cell. Moves a player to the connected teleporter.
+ * @author Liam Date
+ * @version 1.0
+ *
+ */
 public class TeleporterCell extends Cell {
-	public int xLinkedCoord;
-	public int yLinkedCoord;
 	
+	private int xLinkedCoord;
+	private int yLinkedCoord;
+	
+	/**
+	 * Constructor for a teleporter Cell.
+	 * @param xLinkedCoord The x co-ordinate of the linked Teleporter.
+	 * @param yLinkedCoord The y co-ordinate of the linked Teleporter.
+	 */
 	public TeleporterCell (int xLinkedCoord, int yLinkedCoord) {
+		super(CellType.TELEPORTER);
 		this.xLinkedCoord = xLinkedCoord;
 		this.yLinkedCoord = yLinkedCoord;
 	}
 	
+	/**
+	 * Get the x co-ordinate of the linked teleporter.
+	 * @return The x co-ordinate of the linked teleporter.
+	 */
 	public int getLinkedX () {
 		return this.xLinkedCoord;
 	}
 	
+	/**
+	 * Get the y co-ordinate of the linked teleporter.
+	 * @return The y co-ordinate of the linked teleporter.
+	 */
 	public int getLinkedY () {
 		return this.yLinkedCoord;
 	}
