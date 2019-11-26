7791ed7834ab2d76b7ec48825542b5cc454cb0e0
GeorgeManning2000 <58079989+GeorgeManning2000@users.noreply.github.com>
2019-11-26 14:30:23 +0000

    Update StraightLineEnemies.java

--- StraightLineEnemies.java
+++ StraightLineEnemies.java
@@ -1,9 +1,9 @@
 
 public class StraightLineEnemies extends Enemy{
 
-	private String direction;
+	private Direction direction;
 
-	public StraightLineEnemies(int xCoordinate, int yCoordinate, int insertionTime, int height, int width, String direction) {
+	public StraightLineEnemies(int xCoordinate, int yCoordinate, int insertionTime, int height, int width, Direction direction) {
 		super(xCoordinate, yCoordinate, insertionTime, height, width);
 		this.direction=direction;
 	}
