ba96c6a22bfd3e6dfe2dbbc90ab6006040f9de85
DanR-dev <58081171+DanR-dev@users.noreply.github.com>
2019-11-28 16:11:48 +0000

    Added George branch

---
+++ Board.java
@@ -0,0 +1,149 @@
+
+/**
+ * A class that describes the board for each level
+ *
+ * @author George Manning
+ * @version 1.0
+ */
+public class Board {
+
+    private Cell[][] board;
+    private int sizeY;
+    private int sizeX;
+
+    //location of the goal
+    private int goalX;
+    private int goalY;
+
+    /**
+     * The constructor for a board object.
+     *
+     * @param board A 2d array of GameCells that make up the board.
+     * @param x The number of columns the board has.
+     * @param y The number of rows the board has.
+     * @param gx The x-coordinate of the goal.
+     * @param gy The y-coordinate of the goal.
+     */
+    public Board(Cell[][] board, int x, int y, int gx, int gy) {
+        this.board = board;
+        this.sizeX = x;
+        this.sizeY = y;
+        this.goalX = gx;
+        this.goalY = gy;
+    }
+
+    /**
+     * suggested to allow filehandling -Dan.
+     * note: not yet compiled with Cell, not tested.
+     * @param boardData
+     */
+    public Board(String boardData) {
+        String[] splitData = boardData.split(";");
+        String[] cellColumns = splitData[2].split("/");
+        String[][] cells = null;
+
+        for (int i = 0; i < cellColumns.length; i++) {
+            cells[i] = cellColumns[i].split("|");
+        }
+
+        sizeX = cellColumns.length;
+        sizeY = cells[0].length;
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                //add checks and additional code here for special cells
+                board[i][j] = new Cell(cells[i][j]);
+            }
+        }
+    }
+
+    /**
+     * suggested to allow filehandling -Dan.
+     * note: not yet compiled with Cell, not tested.
+     * @return
+     */
+    @Override
+    public String toString() {
+        String boardData = goalX + ";" + goalY + ";";
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                //add checks and additional code here for special cells
+                boardData += board[i][j].toString();
+                if (j < sizeY - 1) {
+                    boardData += "|";
+                }
+            }
+            if (i < sizeX - 1) {
+                boardData += "/";
+            }
+        }
+        return boardData;
+    }
+
+    /**
+     * Updated the selected GameCell into a ground GameCell.
+     *
+     * @param x The x co-ordinate of the GameCell being updated.
+     * @param y The y co-ordinated of the GameCell being updated.
+     */
+    public void updateCell(int x, int y) {
+        board[y][x] = new Cell(CellType.GROUND);
+    }
+
+    /**
+     * Gets a GameCell from the board at a given location.
+     *
+     * @param x The x co-ordinate of the requested GameCell.
+     * @param y The y co-ordinate of the requested GameCell.
+     * @return The GameCell at the given co-ordinate
+     */
+    public Cell getCell(int x, int y) {
+        return this.board[y][x];
+    }
+
+    /**
+     * Get method for the board attribute of the Board class.
+     *
+     * @return The board attribute.
+     */
+    public Cell[][] getBoard() {
+        return this.board;
+    }
+
+    /**
+     * Get method for the number of rows the board has.
+     *
+     * @return The number of rows the board has.
+     */
+    public int getSizeY() {
+        return this.sizeY;
+    }
+
+    /**
+     * Get method for the number or columns the board has.
+     *
+     * @return The number of columns the board has.
+     */
+    public int getSizeX() {
+        return this.sizeX;
+    }
+
+    /**
+     * Get method for the x co-ordinate of the goal.
+     *
+     * @return The x co-ordinate of the goal.
+     */
+    public int getGoalX() {
+        return this.goalX;
+    }
+
+    /**
+     * Get method for the y co-ordinate of the goal/
+     *
+     * @return The y co-ordinate of the goal.
+     */
+    public int getGoalY() {
+        return this.goalY;
+    }
+
+}
---
+++ Player.java
@@ -0,0 +1,204 @@
+
+import javafx.scene.image.Image;
+import java.util.ArrayList;
+
+/**
+ * Class represents the character the user controls.
+ *
+ * @author George Manning
+ * @version 1.0
+ */
+public class Player extends Moveable {
+
+    private ArrayList<Item> inventory;
+    private int tokenNum;
+
+    /**
+     * Comstructor for the player class.
+     *
+     * @param x The initial x co-ordinate of the player.
+     * @param y The initial y co-ordinate of the player.
+     * @param image The image used to represent the player.
+     */
+    public Player(int x, int y) {
+        super(x, y);
+        this.inventory = new ArrayList<Item>();
+        this.tokenNum = 0;
+    }
+
+    /**
+     * suggested to allow filehandling -Dan. note: if player image is to be set
+     * here as suggested in Moveable() then that should be added here, not
+     * tested.
+     *
+     * @param playerData
+     */
+    public Player(String playerData) {
+        super(playerData);
+        String playerObjData = playerData.split(";")[1];
+        String[] splitData = playerObjData.split("/");
+
+        tokenNum = Integer.parseInt(splitData[0]);
+
+        for (int i = 1; i < splitData.length; i++) {
+            if (splitData[i] != null) {
+                inventory.add(Item.valueOf(splitData[i]));
+            }
+        }
+    }
+
+    /**
+     * suggested to allow filehandling -Dan. note: untested.
+     *
+     * @param playerData
+     * @return
+     */
+    @Override
+    public String toString() {
+        String playerData = super.toString() + ";";
+        playerData += Integer.toString(tokenNum);
+        Item[] inventory = (Item[]) this.inventory.toArray();
+
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i] != null) {
+                playerData += "/" + inventory[i].toString();
+            }
+        }
+        return playerData;
+    }
+
+    /**
+     * Moves the player in a user specified direction
+     *
+     * @param direction The direction to be moved to
+     */
+    public void move(Direction direction, Board board) {
+
+        Cell nextCell = super.getNextCell(direction, board);
+        if (this.isMoveValid(nextCell)) {
+
+            super.move(direction);
+            this.pickUpItem(nextCell.getType());
+            this.removeItem(nextCell.getType());
+
+            if (nextCell.getType().equals(CellType.TELEPORTER)) { //what happens when you step on a teleporter
+
+                Teleporter teleporter = (Teleporter) board.getCell(this.xCoord, this.yCoord);
+                //method names may be wrong
+                super.setPosition(teleporter.getLinkedX(), teleporter.getLinkedY());
+            } else if (nextCell.getType().equals(CellType.TOKEN_DOOR)) {
+
+                TokenDoor tokenDoor = (TokenDoor) board.getCell(this.xCoord, this.yCoord);
+                this.removeTokens(tokenDoor.getNumTokensNeeded());
+
+            }
+
+        }
+    }
+
+    /**
+     * Works out if a move is valid.
+     *
+     * @return True is the move is valid, fakse otherwise.
+     */
+    public boolean isMoveValid(Cell cell) {
+        CellType type = cell.getType();
+        switch (type) {
+            case WALL:
+                return false;
+            case WATER:
+                if (this.hasItem(Item.FLIPPERS)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case FIRE:
+                if (this.hasItem(Item.FIREBOOTS)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case RED_DOOR:
+                if (this.hasItem(Item.RED_KEY)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case BLUE_DOOR:
+                if (this.hasItem(Item.BLUE_KEY)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case GREEN_DOOR:
+                if (this.hasItem(Item.GREEN_KEY)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case TOKEN_DOOR:
+                TokenDoor tCell = (TokenDoor) cell;
+                if (tCell.getNumTokensNeeded() >= this.tokenNum) { //need token door to test
+                    return true;
+                } else {
+                    return false;
+                }
+            default:
+                return true;
+
+        }
+    }
+
+    private void addToken() {
+        tokenNum += 1;
+    }
+
+    private void removeTokens(int n) {
+        tokenNum -= n;
+    }
+
+    private void pickUpItem(CellType type) {
+        switch (type) {
+            case FLIPPERS:
+                inventory.add(Item.FLIPPERS);
+                break;
+            case FIREBOOTS:
+                inventory.add(Item.FIREBOOTS);
+                break;
+            case RED_KEY:
+                inventory.add(Item.RED_KEY);
+                break;
+            case BLUE_KEY:
+                inventory.add(Item.BLUE_KEY);
+                break;
+            case GREEN_KEY:
+                inventory.add(Item.GREEN_KEY);
+                break;
+            case TOKEN:
+                addToken();
+            default:
+                break; //do nothing
+        }
+    }
+
+    private boolean hasItem(Item item) {
+        return this.inventory.contains(item);
+    }
+
+    private void removeItem(CellType type) {
+        switch (type) {
+            case RED_DOOR:
+                inventory.remove(Item.RED_KEY);
+                break;
+            case BLUE_DOOR:
+                inventory.remove(Item.BLUE_KEY);
+                break;
+            case GREEN_DOOR:
+                inventory.remove(Item.GREEN_KEY);
+                break;
+            default:
+                break; //do nothing
+        }
+    }
+
+}
